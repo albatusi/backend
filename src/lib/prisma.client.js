db4d06177788edb3568d420cdc6aad1d8b4068df
Albatusi <albatusi009@gmail.com>
2025-10-01 01:41:23 -0500

    actualizacion de errores

--- src/lib/prisma.client.js
+++ src/lib/prisma.client.js
@@ -5,7 +5,7 @@ const prisma = global.prisma || new PrismaClient({
     log: ["query"], // Opcional: Para ver los logs de las consultas SQL
 });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     global.prisma = prisma;
 }
 
